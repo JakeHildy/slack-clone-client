b68065628461ede7ce5486ba2b4fe708b2d5f542
Jacob Hildebrandt <jake@hildy.ca>
2021-06-01 10:59:02 -0700

    Styled Rooms Section

--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -2,7 +2,36 @@ import React from "react";
 import "./Rooms.scss";
 
 function Rooms() {
-  return <div className="rooms">rooms</div>;
+  const joinRoom = (e) => {
+    e.preventDefault();
+    console.log("join room clicked");
+  };
+
+  return (
+    <div className="rooms">
+      <h3 className="rooms__title">Rooms</h3>
+      <ul className="rooms__list">
+        <li className="rooms__list-item" onClick={(e) => joinRoom(e)}>
+          <span
+            className="rooms__icon glyphicon glyphicon-lock"
+            aria-hidden="true"
+          >
+            {" "}
+          </span>
+          Main Room
+        </li>
+        <li className="rooms__list-item" onClick={(e) => joinRoom(e)}>
+          <span
+            className="rooms__icon glyphicon glyphicon-globe"
+            aria-hidden="true"
+          >
+            {" "}
+          </span>
+          Meeting Room Room
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Rooms;
