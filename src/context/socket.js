ed195d4ab350acefe287218869b8b066568781c6
Jacob Hildebrandt <jake@hildy.ca>
2021-06-05 10:10:29 -0700

    Base Slack Functionality Complete :) Yay!

--- src/context/socket.js
+++ src/context/socket.js
@@ -2,8 +2,15 @@ import React from "react";
 import socketio from "socket.io-client";
 export const SOCKET_URL = "http://192.168.1.75:9000";
 
+let username = prompt("What is your username?");
+while (!username) {
+  username = prompt("Invalid username. What is your username?");
+}
+
 const SocketContext = React.createContext();
 SocketContext.displayName = "SocketContext";
 
-export const socket = socketio.connect(SOCKET_URL);
+export const socket = socketio.connect(SOCKET_URL, {
+  query: { username },
+});
 export default SocketContext;
