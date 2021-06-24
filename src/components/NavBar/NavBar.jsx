df28154105a4be9441ff384cf8b0a47aa714ea87
Jacob <jacob.hildebrandt@gmail.com>
2021-06-23 18:24:52 -0700

    Refactored NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,24 +7,27 @@ import CustomAvatarSmall from "./../CustomAvatarSmall/CustomAvatarSmall";
 function NavBar({ handleOpenSettings, handleLogout }) {
   let userContext = useContext(UserContext);
 
+  const userProfile = () => {
+    const { username, avatarConfig } = userContext.userData;
+    return (
+      <>
+        <h3 className="navbar__username">{username}</h3>
+        <div className="navbar__logout" onClick={handleLogout}>
+          logout
+        </div>
+        <div className="navbar__avatar">
+          <CustomAvatarSmall avatarConfig={avatarConfig} />
+        </div>
+      </>
+    );
+  };
+
   return (
     <div className="navbar">
       <div className="navbar__icon" onClick={handleOpenSettings}>
         <SettingsIcon fill="#fff" />
       </div>
-      {userContext.userData && (
-        <>
-          <h3 className="navbar__username">{userContext.userData.username}</h3>
-          <div className="navbar__logout" onClick={handleLogout}>
-            logout
-          </div>
-          <div className="navbar__avatar">
-            <CustomAvatarSmall
-              avatarConfig={userContext.userData.avatarConfig}
-            />
-          </div>
-        </>
-      )}
+      {userContext.userData && userProfile()}
     </div>
   );
 }
