ba445b06c358d7e6b59c2a601d84e349fbb38112
Jacob <jacob.hildebrandt@gmail.com>
2021-07-05 14:06:38 -0700

    socket saving changes to file

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,6 +1,6 @@
 import React from "react";
-import socketio from "socket.io-client";
-export const SOCKET_URL = "http://192.168.1.72:9000";
+// import socketio from "socket.io-client";
+// export const SOCKET_URL = "http://192.168.1.72:9000";
 
 // const id = sessionStorage.getItem("id");
 
